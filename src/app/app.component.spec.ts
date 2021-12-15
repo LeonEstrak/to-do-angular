af8845845e65a198f209e910777c7959f50aaa07
LeonEstrak <aniket2583@gmail.com>
2021-12-15 21:35:07 +0530

    Failing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -11,7 +11,7 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(app).toBeFalsy();
   });
 
   it(`should have as title 'to-do-angular'`, () => {
